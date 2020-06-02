2300061575aa77453a0cd677aec44d133187e053
funkemi <funkeoluleye@gmail.com>
2020-06-02 21:02:26 +0100

    Add files via upload

---
+++ myscript.py
@@ -0,0 +1,4 @@
+full_name = " Olufunke John-Oluleye"
+hngi7_id = "HNG-02080"
+language = "Javascript"
+print("Hello World, this is " + full_name + " with HNGi7 ID " + hngi7_id + " using " + language + " for stage 2 task")
